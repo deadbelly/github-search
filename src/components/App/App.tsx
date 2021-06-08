289678677edc83b25c33dcaab93d7fec414589f1
Boone Epstein <booneepstein@gmail.com>
2021-06-08 13:50:45 -0500

    Add App state and Repo interface

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,5 @@
+import React, { useState } from 'react'
+
 //components
 import { RepoCard } from '../RepoCard/RepoCard'
 import { RepoDetails } from '../RepoDetails/RepoDetails'
@@ -5,9 +7,52 @@ import { SearchBar } from '../SearchBar/SearchBar'
 import { SearchResults } from '../SearchResults/SearchResults'
 
 //styles
-import './App.css';
+import './App.css'
 
-export const App = () => {
+interface Repo {
+  id: number,
+  name: string,
+  fullName: string,
+  ownerName: string,
+  ownerAviUrl: string,
+  ownerProfUrl: string,
+  ownerType: string,
+  isPrivate: boolean,
+  repoUrl: string,
+  repoDescrip: string,
+  isFork: boolean,
+  url: string,
+  createdAt: Date,
+  updatedAt: Date,
+  pushedAt: Date,
+  size: number,
+  stargazersCount: number,
+  watchersCount: number,
+  language: string,
+  forksCount: number,
+  openIssuesCount: number,
+  mainBranch: string,
+  defaultBranch: string,
+  score: number,
+  forks: number,
+  openIssues: number,
+  watchers: number,
+  hasIssues: boolean,
+  hasProjects: boolean,
+  hasPages: boolean,
+  hasWiki: boolean,
+  hasDownloads: boolean,
+  isArchived: boolean,
+  isDisabled: boolean,
+  licenseName?: string,
+  licenseUrl?: string,
+}
+
+export const App: React.FC = () => {
+  const [allResults, setAllResults] = useState<Array<Repo>>([])
+  const [filters, setFilters] = useState({})
+  const [selection, setSelection] = useState<Repo | null>(null)
+
   return (
     <div className="App">
       PLACEHOLDER
