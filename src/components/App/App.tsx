90fff6851311be3f5900b92ce9cd63864a6fa15e
Boone Epstein <booneepstein@gmail.com>
2021-06-08 11:45:47 -0500

    Set up template architecture

---
+++ src/components/App/App.tsx
@@ -0,0 +1,18 @@
+//components
+import { RepoCard } from '../RepoCard/RepoCard'
+import { RepoDetails } from '../RepoDetails/RepoDetails'
+import { SearchBar } from '../SearchBar/SearchBar'
+import { SearchResults } from '../SearchResults/SearchResults'
+
+//styles
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      PLACEHOLDER
+    </div>
+  );
+}
+
+export default App;
