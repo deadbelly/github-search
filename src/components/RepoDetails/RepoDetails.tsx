2c66dfa821225751958826b0f38d7fa99f9ca3d4
Boone Epstein <booneepstein@gmail.com>
2021-06-09 13:52:58 -0500

    Build out boilerplate RepoDetails and add types

--- src/components/RepoDetails/RepoDetails.tsx
+++ src/components/RepoDetails/RepoDetails.tsx
@@ -1,3 +1,18 @@
-export const RepoDetails = () => {
+import React from 'react'
+import { Repo } from '../App/App'
+
+
+interface Props {
+  ownerName: string
+  name: string
+  allResults: Repo[]
+}
+
+export const RepoDetails: React.FC<Props> = ({ ownerName, name, allResults}) => {
+
+  return (
+    <>
+    </>
+  )
 
 }
