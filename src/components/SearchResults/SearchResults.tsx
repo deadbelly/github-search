c2ff6ac7172d5893f6f297ff5fba5c5ecc9fb672
Boone Epstein <booneepstein@gmail.com>
2021-06-09 12:00:43 -0500

    Build out types and state framework for Search Results

--- src/components/SearchResults/SearchResults.tsx
+++ src/components/SearchResults/SearchResults.tsx
@@ -1,3 +1,19 @@
-export const SearchResults = () => {
+import React, { useState } from 'react'
+import { Repo, Filters } from '../App/App'
+import { useFilters } from './hooks'
 
+interface Props {
+  allResults: Repo[]
+  filters: Filters
+}
+
+export const SearchResults: React.FC<Props> = ({ allResults, filters }) => {
+  const [filteredResults, setFilteredResults] = useState<Repo[]>([])
+
+  
+
+  return (
+    <>
+    </>
+  )
 }
