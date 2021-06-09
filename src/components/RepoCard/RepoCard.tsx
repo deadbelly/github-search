6d4017b783587e012bfb7e986f8360de91e3168a
Boone Epstein <booneepstein@gmail.com>
2021-06-09 12:14:33 -0500

    Build boilerplate RepoCard

--- src/components/RepoCard/RepoCard.tsx
+++ src/components/RepoCard/RepoCard.tsx
@@ -1,3 +1,12 @@
-export const RepoCard = () => {
+import React from 'react'
+import { Repo } from '../App/App'
 
+export const RepoCard: React.FC<Repo> = ({ repo }) => {
+  return (
+    <div className='repo-card'>
+      <h2>{repo.name}</h2>
+      <h3>{repo.owner}</h3>
+      <h3>{repo.language}</h3>
+    </div>
+  )
 }
