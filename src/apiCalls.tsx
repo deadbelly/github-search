b2c22f170d74a7a09464b851e20dd9dddd1ade41
Boone Epstein <booneepstein@gmail.com>
2021-06-10 15:18:38 -0500

    Remove need for api key

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -1,11 +1,6 @@
 import { Repo } from './components/App/App'
 import { cleanRepoData } from './dataCleaning'
 
-
-const authHeader = {
-  headers: { authorization: `token ${process.env.REACT_APP_GH_KEY}` }
-}
-
 const checkResponse = (response: any): any => {
   if (response.ok) {
     return response.json()
@@ -15,13 +10,13 @@ const checkResponse = (response: any): any => {
 }
 
 export const getRepos = (query: string, sort: string): Promise<Repo[]> => {
-  return fetch(`https://api.github.com/search/repositories?q=${query}&sort=${sort}`, authHeader)
+  return fetch(`https://api.github.com/search/repositories?q=${query}&sort=${sort}`)
     .then(response => checkResponse(response))
       .then(data => data.items.map((repo: any) => cleanRepoData(repo)))
 }
 
 export const getSingleRepo = (ownerName: string, name: string): Promise<Repo> => {
-  return fetch(`https://api.github.com/repos/${ownerName}/${name}`, authHeader)
+  return fetch(`https://api.github.com/repos/${ownerName}/${name}`)
     .then(response => checkResponse(response))
       .then(data => cleanRepoData(data))
 }
